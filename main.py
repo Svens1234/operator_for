54488376ad68f11fda2112ce38d8bcadcd6d223a
Svenden123 <buhrus1@yandex.ru>
2021-08-03 16:14:10 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+#number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#for number in number_list:
+    #if number % 2 == 0:
+        #print(number)
+    #else:
+        #print('Hey!')
+
+
+#number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#for number in number_list:
+    #print(str(number) + 'Hola!')
+
+
+#number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#list_numbers_sum = 0
+#for number in number_list:
+    #list_numbers_sum = list_numbers_sum + number
+#print(list_numbers_sum)
+
+
+#greeting = 'Hello Python!'
+#for letter in greeting:
+    #print(letter)
+
+
+#greeting = 'Hello Python!'
+#for letter in greeting:
+    #if letter != 'o':
+        #print(letter)
+
+
+#for letter in 'Hello Python!':
+    #if letter != 'o':
+        #print(letter)
+
+
+#for letter in 'Hello Python!':
+        #print('One more letter')
+
+
+#tuple_list = [('a', 'b'), ('c', 'd'), ('e', 'f')]
+#for item in tuple_list:
+    #print(item)
+#for letter_1, letter_2 in tuple_list:
+    #print(letter_1, letter_2)
+#for letter_1, letter_2 in tuple_list:
+    #print(letter_1)
+
+
+#tuple_list_1 = [('a', 'b', 1), ('c', 'd', 4), ('e', 'f', 5)]
+#for letter_1, letter_2, number in tuple_list_1:
+    #print(letter_1, letter_2, number)
+
+
+#dictionary = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+#for item in dictionary:
+    #print(item)
+#for item in dictionary.items():
+    #print(item)
+#for item in dictionary.keys():
+    #print(item)
+#for item in dictionary.values():
+    #print(item)
+#for key, value in dictionary.items():
+    #print(key,value)
+
+for x in range(5):
+    print('Hello!')
+
+
+
+
